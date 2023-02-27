a102c0a7ae7b43d486eedb04ac43d6c9e0f7b662
Sirisatri <sirisatri.k@kkumail.com>
2023-02-27 18:48:04 +0700

    adding2

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
           body: SafeArea(
-        child: SignupPage(),
+        child: Adding(),
       )),
     );
   }
