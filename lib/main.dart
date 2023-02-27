49afa5f7ddd5f4fced137b98448e139b6befcb7c
Sirisatri <sirisatri.k@kkumail.com>
2023-02-27 15:31:43 +0700

    ddd

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'app_screens/login_page.dart';
+import 'app_screens/price.dart';
+import 'app_screens/detailpage.dart';
+import 'app_screens/images_slider.dart';
+import 'app_screens/signup_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: SafeArea(
+        child: SignupPage(),
+      )),
+    );
+  }
+}
